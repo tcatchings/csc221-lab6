50aef14d38e926a1ae954cb619ac31472314a02b
David O'Gwynn <dogwynn@acm.org>
2016-03-22 12:09:50 -0500

    added tests

--- lab6.py
+++ lab6.py
@@ -25,8 +25,44 @@ def front_x(words):
 # [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # 
-# Hint: use a custom key= function to extract the last element form
+# Hint: use a custom key= function to extract the last element from
 # each tuple.
 def sort_last(tuples):
     return
 
+
+
+def test_match_ends():
+    from nose.tools import assert_equal 
+    assert_equal(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+    assert_equal(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+    assert_equal(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+def test_front_x():
+    from nose.tools import assert_equal 
+    assert_equal(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+                 ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    assert_equal(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+                 ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    assert_equal(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+                 ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+
+def test_sort_last():
+    from nose.tools import assert_equal 
+    assert_equal(sort_last([(1, 3), (3, 2), (2, 1)]),
+                 [(2, 1), (3, 2), (1, 3)])
+    assert_equal(sort_last([(2, 3), (1, 2), (3, 1)]),
+                 [(3, 1), (1, 2), (2, 3)])
+    assert_equal(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+                 [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+    
+
+
+def main():
+    test_match_ends()
+    test_front_x()
+    test_sort_last()
+
+if __name__ == '__main__':
+    main()
